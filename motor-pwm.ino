172d07e3032c5fcfd78a0bc1e74dc6a831cd8e1f
Kauê Rodrigues <kauerodrigues98@gmail.com>
2020-09-13 16:23:05 -0300

    code improvements.

--- motor-pwm.ino
+++ motor-pwm.ino
@@ -24,65 +24,65 @@
 #define PIN_MOTOR_1_PWM 11
 
 void setup() {
-	pinMode(PIN_DS, OUTPUT);
-	pinMode(PIN_SH_CP, OUTPUT);
-	pinMode(PIN_ST_CP, OUTPUT);
-	pinMode(PIN_ENABLE, OUTPUT);
+    pinMode(PIN_DS, OUTPUT);
+    pinMode(PIN_SH_CP, OUTPUT);
+    pinMode(PIN_ST_CP, OUTPUT);
+    pinMode(PIN_ENABLE, OUTPUT);
 
-	pinMode(PIN_MOTOR_1_PWM, OUTPUT);
-	pinMode(PIN_MOTOR_2_PWM, OUTPUT);
-	pinMode(PIN_MOTOR_3_PWM, OUTPUT);
-	pinMode(PIN_MOTOR_4_PWM, OUTPUT);
+    pinMode(PIN_MOTOR_1_PWM, OUTPUT);
+    pinMode(PIN_MOTOR_2_PWM, OUTPUT);
+    pinMode(PIN_MOTOR_3_PWM, OUTPUT);
+    pinMode(PIN_MOTOR_4_PWM, OUTPUT);
 
-	digitalWrite(PIN_ENABLE, LOW);
+    digitalWrite(PIN_ENABLE, LOW);
 
-	setMotorStatusByPin(BIT_MOTOR_1_B, LOW);
-	setMotorStatusByPin(BIT_MOTOR_1_A, HIGH);
+    setMotorStatusByPin(BIT_MOTOR_1_B, LOW);
+    setMotorStatusByPin(BIT_MOTOR_1_A, HIGH);
 
-	setMotorStatusByPin(BIT_MOTOR_2_B, LOW);
-	setMotorStatusByPin(BIT_MOTOR_2_A, HIGH);
+    setMotorStatusByPin(BIT_MOTOR_2_B, LOW);
+    setMotorStatusByPin(BIT_MOTOR_2_A, HIGH);
 
-	setMotorStatusByPin(BIT_MOTOR_3_B, LOW);
-	setMotorStatusByPin(BIT_MOTOR_3_A, HIGH);
+    setMotorStatusByPin(BIT_MOTOR_3_B, LOW);
+    setMotorStatusByPin(BIT_MOTOR_3_A, HIGH);
 
-	setMotorStatusByPin(BIT_MOTOR_4_B, LOW);
-	setMotorStatusByPin(BIT_MOTOR_4_A, HIGH);
+    setMotorStatusByPin(BIT_MOTOR_4_B, LOW);
+    setMotorStatusByPin(BIT_MOTOR_4_A, HIGH);
 }
 
 void setMotorStatusByPin(byte pin, bool status) {
-	static byte ciBuffer[QUANTITY_OF_CI];
+    static byte ciBuffer[QUANTITY_OF_CI];
 
-	bitWrite(ciBuffer[pin / 8], pin % 8, status);
+    bitWrite(ciBuffer[pin / 8], pin % 8, status);
 
-	digitalWrite(PIN_DS, LOW);
-	digitalWrite(PIN_SH_CP, LOW);
-	digitalWrite(PIN_ST_CP, LOW);
+    digitalWrite(PIN_DS, LOW);
+    digitalWrite(PIN_SH_CP, LOW);
+    digitalWrite(PIN_ST_CP, LOW);
 
-	for (int nC = QUANTITY_OF_CI - 1; nC >= 0; nC--) {
-		for (int nB = 7; nB >= 0; nB--) {
-			digitalWrite(PIN_SH_CP, LOW); 
-			digitalWrite(PIN_DS, bitRead(ciBuffer[nC], nB));
-			digitalWrite(PIN_SH_CP, HIGH);
-			digitalWrite(PIN_DS, LOW);     
-		}  
-	}
+    for (int nC = QUANTITY_OF_CI - 1; nC >= 0; nC--) {
+        for (int nB = 7; nB >= 0; nB--) {
+            digitalWrite(PIN_SH_CP, LOW);
+            digitalWrite(PIN_DS, bitRead(ciBuffer[nC], nB));
+            digitalWrite(PIN_SH_CP, HIGH);
+            digitalWrite(PIN_DS, LOW);
+        }
+    }
 
-	digitalWrite(PIN_ST_CP, HIGH);
+    digitalWrite(PIN_ST_CP, HIGH);
 }
 
 void delayPWM(unsigned long maxTime) {
-	byte valueA0;
-	unsigned long start = millis();
+    byte valueA0;
+    unsigned long start = millis();
 
-	while ((millis() - start) < maxTime) {
-		valueA0 = map(analogRead(ANALOG_PIN), 0, 1023, 0, 255);
-		analogWrite(PIN_MOTOR_1_PWM, valueA0);
-		analogWrite(PIN_MOTOR_2_PWM, valueA0);
-		analogWrite(PIN_MOTOR_3_PWM, valueA0);
-		analogWrite(PIN_MOTOR_4_PWM, valueA0);
-	}
+    while ((millis() - start) < maxTime) {
+        valueA0 = map(analogRead(ANALOG_PIN), 0, 1023, 0, 255);
+        analogWrite(PIN_MOTOR_1_PWM, valueA0);
+        analogWrite(PIN_MOTOR_2_PWM, valueA0);
+        analogWrite(PIN_MOTOR_3_PWM, valueA0);
+        analogWrite(PIN_MOTOR_4_PWM, valueA0);
+    }
 }
 
 void loop() {
-	delayPWM(2000);
+    delayPWM(2000);
 }
